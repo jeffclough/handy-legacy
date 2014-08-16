53996cfb0958a28fd194decf4e94231f6f381c2e
Jeff Clough <jeff@cloughcottage.com>
2014-08-16 00:49:10 -0400

    Corrected bug in pwgen so that -d option (--dictionary).
    I've also split the pa command into two parts, once of which is a Python
    module that can provide phonetic alphabet functionality to other
    programs.

---
+++ pylib/phonetics.py
@@ -0,0 +1,30 @@
+import optparse,os,sys
+
+dialect=dict(
+  nato=dict([(w[0].lower(),w) for w in 'Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey Xray Yankee Zulu'.split()]),
+  raf=dict([(w[0].lower(),w) for w in 'Able/Affirm Baker Charlie Dog Easy Fox George How Item/Interrogatory Jig/Johny King Love Mike Nab/Negat Oboe Peter/Prep Queen Roger Sugar Tare Uncle Victor William X-ray Yoke Zebra'.split()]),
+  police=dict([(w[0].lower(),w) for w in 'Adam Boy Charlie David Edward Frank George Henry Ida John King Lincoln Mary Nora Ocean Peter Queen Robert Sam Tom Union Victor William X-ray Young Zebra'.split()]),
+  us=dict([(w[0].lower(),w) for w in 'Able Baker Charlie Dog Easy Fox George How Item Jig King Love Mike Nan Oboe Peter Queen Roger Sugar Tare Uncle Victor William X-ray Yoke Zebra'.split()]),
+  wu=dict([(w[0].lower(),w) for w in 'Adams Boston Chicago Denver Easy Frank George Henry Ida John King Lincoln Mary NewYork Ocean Peter Queen Roger Sugar Thomas Union Victor William X-ray Young Zero'.split()])
+)
+
+dialects=' '.join(sorted(dialect.keys()))
+
+def wordify(s,**kwargs):
+  """Return the phonetic-alphabetic translation of the given string.
+
+  Keyword Arguments:
+  dialect - Valid values are nato (the default), raf, police, us, and wu.
+  ignore_case - True if all characters are to be regarded as lower case."""
+
+  d=dialect[kwargs.get('dialect','nato')]
+  ic=kwargs.get('ignore_case',False)
+
+  output=[]
+  for word in s.split():
+    for ch in word:
+      w=d.get(ch.lower(),ch)
+      if not ic and ch.isupper():
+        w=w.upper()
+      output.append(w)
+  return ' '.join(output)
