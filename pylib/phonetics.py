30a3ca3d850eb53f442a97b7c212455dda26cbca
Jeff Clough <jeff@cloughcottage.com>
2015-08-08 14:07:35 -0400

    The pa script now supports -l and -L options.
    -l  Just list the default or given phonetic dialect.
    -L  List words from all available dialects in tabular form.

--- pylib/phonetics.py
+++ pylib/phonetics.py
@@ -2,7 +2,7 @@ import optparse,os,sys
 
 dialect=dict(
   nato=dict([(w[0].lower(),w) for w in 'Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey Xray Yankee Zulu'.split()]),
-  raf=dict([(w[0].lower(),w) for w in 'Able/Affirm Baker Charlie Dog Easy Fox George How Item/Interrogatory Jig/Johny King Love Mike Nab/Negat Oboe Peter/Prep Queen Roger Sugar Tare Uncle Victor William X-ray Yoke Zebra'.split()]),
+  raf=dict([(w[0].lower(),w) for w in 'Affirm Baker Charlie Dog Easy Fox George How Interrogatory Johny King Love Mike Nab Oboe Prep Queen Roger Sugar Tare Uncle Victor William X-ray Yoke Zebra'.split()]),
   police=dict([(w[0].lower(),w) for w in 'Adam Boy Charlie David Edward Frank George Henry Ida John King Lincoln Mary Nora Ocean Peter Queen Robert Sam Tom Union Victor William X-ray Young Zebra'.split()]),
   us=dict([(w[0].lower(),w) for w in 'Able Baker Charlie Dog Easy Fox George How Item Jig King Love Mike Nan Oboe Peter Queen Roger Sugar Tare Uncle Victor William X-ray Yoke Zebra'.split()]),
   wu=dict([(w[0].lower(),w) for w in 'Adams Boston Chicago Denver Easy Frank George Henry Ida John King Lincoln Mary NewYork Ocean Peter Queen Roger Sugar Thomas Union Victor William X-ray Young Zero'.split()])
