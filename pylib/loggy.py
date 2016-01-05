f6b102511226aad0c30e9866cc4ad756d0bcbb84
Jeff Clough <jeff@cloughcottage.com>
2016-01-04 23:07:42 -0500

    loggy.get_logger()'s facility can now be a handler
    The facility argument can now be a instance of logging.Handler or any
    subclass thereof, in addition of all the other types that were already
    supported.
    
    loggy.get_logger()'s arguments are now all keyword arguments.

--- pylib/loggy.py
+++ pylib/loggy.py
@@ -19,13 +19,24 @@ def get_log_level_by_name(level):
       return logging._levelNames[level]
   raise ValueError,'bad log level value: %r'%(level,)
 
-def get_logger(facility=None,level='WARNING',name=os.path.basename(sys.argv[0]).split('.')[0],logfmt='%(name)s %(levelname).1s: %(message)s',datefmt='%Y-%m-%d %H:%M:%S '):
+def get_logger(**kwargs):
   """Return the default logging object (if facility==None), or set up a
-  new logger in any other case, and return that.
+  new logger in any other case, and return that. Keyword arguments and
+  their default values are:
+
+    facility  None
+    level     'warning'
+    name      basename of process, minus any extension
+    logfmt    '%(name)s %(levelname).1s: %(message)s'
+    datefmt   '%Y-%m-%d %H:%M:%S '
+
+  Note the space at the end of datefmt's default value. None is proviced
+  automatically, so end with one if you want one there.
 
   The facility argument, if not None, may be a filename (e.g.
-  "$HOME/myprog.log"), a file stream (e.g. sys.stderr), or an integer
-  value from SysLogHandler's LOG_* values (e.g. SysLogHandler.LOG_USER).
+  "$HOME/myprog.log"), a file stream (e.g. sys.stderr), an integer
+  value from SysLogHandler's LOG_* values (e.g. SysLogHandler.LOG_USER),
+  or an instance of logging.Handler or any subclass thereof.
 
   The level argument should be one of the following string values:
   DEBUG, INFO, NOTICE, WARNING, or ERR. (WARNING is the default.)
@@ -43,38 +54,56 @@ def get_logger(facility=None,level='WARNING',name=os.path.basename(sys.argv[0]).
   separator) if you want the to separate the timestamp from the rest of
   the logged message."""
 
+  # Instantiate our keyword arguments as local variables.
+  facility=kwargs.get('facility',None)
+  level=kwargs.get('level','warning')
+  name=kwargs.get('name',os.path.basename(sys.argv[0]).split('.')[0])
+  logfmt=kwargs.get('logfmt','%(name)s %(levelname).1s: %(message)s')
+  datefmt=kwargs.get('datefmt','%Y-%m-%d %H:%M:%S ')
+
   if facility==None:
     # Assume this process has already set up a logger (or just wants to
     # use the default logger), and return that.
     return logging.getLogger()
 
-  if isinstance(facility,basestring):
-    if facility in syslog_facilities:
-      # It looks like we're logging to syslog.
-      facility=SysLogHandler.facility_names[facility]
+  h=None
+  if isinstance(facility,logging.Handler):
+    # The caller has provided us with his own handler.
+    h=facility
+    if isinstance(h,logging.StreamHandler):
+      # Include the date and time in our log format.
+      f=logging.Formatter('%(asctime)s'+logfmt,datefmt=datefmt)
     else:
-      # This string must be a filename, so open it for appending.
-      facility=open(facility,'a')
-
-  if isinstance(facility,int):
-    # This is a syslog facility number, or had better be.
-    system=platform.system()
-    if system=='Darwin':
-      h=SysLogHandler(address='/var/run/syslog',facility=facility)
-    elif system=='Linux':
-      h=SysLogHandler(address='/dev/log',facility=facility)
-    else:
-      h=SysLogHandler(
-        address=('localhost',logging.handlers.SYSLOG_UDP_PORT),
-        facility=facility
-      )
-    f=logging.Formatter(logfmt)
-  elif isinstance(facility,file):
-    # This is a stream, so set up formatting accordingly.
-    h=logging.StreamHandler(facility)
-    f=logging.Formatter('%(asctime)s'+logfmt,datefmt=datefmt)
+      # Trust the logging service to provide current date and time.
+      f=logging.Formatter(logfmt)
   else:
-    raise ValueError,'bad log facility value: %r'%(facility,)
+    if isinstance(facility,basestring):
+      if facility in syslog_facilities:
+        # It looks like we're logging to syslog.
+        facility=SysLogHandler.facility_names[facility]
+      else:
+        # This string must be a filename, so open it for appending.
+        facility=open(facility,'a')
+
+    if isinstance(facility,int):
+      # This is a syslog facility number, or had better be.
+      system=platform.system()
+      if system=='Darwin':
+        h=SysLogHandler(address='/var/run/syslog',facility=facility)
+      elif system=='Linux':
+        h=SysLogHandler(address='/dev/log',facility=facility)
+      else:
+        h=SysLogHandler(
+          address=('localhost',logging.handlers.SYSLOG_UDP_PORT),
+          facility=facility
+        )
+      f=logging.Formatter(logfmt)
+    elif isinstance(facility,file):
+      # This is a stream, so set up formatting accordingly.
+      h=logging.StreamHandler(facility)
+      f=logging.Formatter('%(asctime)s'+logfmt,datefmt=datefmt)
+    else:
+      raise ValueError,'bad log facility value: %r'%(facility,)
 
   if isinstance(level,basestring):
     # If level is a string, make sure it is upper case.
@@ -105,5 +134,5 @@ if __name__=='__main__':
   if opt.facility=='stdout':   opt.facility=sys.stdout
   elif opt.facility=='stderr': opt.facility=sys.stderr
 
-  log=get_logger(opt.facility,opt.level)
+  log=get_logger(facility=opt.facility,level=opt.level)
   log.log(logging._levelNames[opt.level.upper()],' '.join(args))
