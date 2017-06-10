04c9d26b4583c7f807d2ed4571acac43bbd8c4d4
Jeff Clough <jeff@cloughcottage.com>
2017-06-10 00:50:37 -0400

    Refined GroupParser
    The ending group index now points one character past the end of the
    group. This makes the (start,end) pairs suitable for slicing the parsed
    text.
    
    Replaced length() method with __len__(), which seems to actually work.
    
    Added a groups() method that returns index pairs for slicing the parsed
    text.

--- pylib/parsing.py
+++ pylib/parsing.py
@@ -1,20 +1,29 @@
+#!/usr/bin/env python
 
 # Lengthable is to be used as a metaclass of classes that support
 # len(instance) calls.
-class Lengthable(type):
-  def __len__(self):
-    return self.length()
+#class Lengthable(type):
+#  def __len__(self):
+#    return self.length()
 
 class GroupParser(object):
+
+  # Enable len(some_GroupParser_object) to work.
+  #__metaclass__=Lengthable
 
   # GroupParser.Error exceptions are raised whenn things go wrong.
   class Error(Exception):
     pass
 
-  # Enable len(some_GroupParser_object) to work.
-  __metaclass__=Lengthable
-
   def __init__(self,**kwargs):
+    """Create and initialize a GroupParser object.
+
+    groupers:     A sequence of 2-character strings, pairs of characters
+                  describing what characters can be used to group text.
+                  Default: ('()', '[]', '{}')
+    escape_char:  The character used to escape any of the grouping
+                  characters in order to suppress their grouping
+                  function."""
   
     self.groupers=kwargs.get('groupers',('()','[]','{}'))
     self.escape_char=kwargs.get('escape_char',None)
@@ -27,18 +36,31 @@ class GroupParser(object):
     self.text=''
     self.next_index=0
 
-  def length(self): 
+  def __len__(self): 
     "Return the number of bracketed groups parsed from our text."
 
     return len(self.parens)
+
+  #def length(self): 
+  #  "Return the number of bracketed groups parsed from our text."
+  #
+  #  return len(self.parens)
 
   def __getitem__(self,i):
     """Return the (start,stop,group_text) tuple at index i of our list
     of bracketted groups."""
 
     return tuple(
-      self.parens[i]+[self.text[self.parens[i][0]:self.parens[i][1]+1]]
+      self.parens[i]+[self.text[self.parens[i][0]:self.parens[i][1]]]
     )
+
+  def groups(self):
+    """"Return a list of (start,stop) pairs describing the location of
+    each bracketed group within the parsed string. If the The start and
+    stop values are used to slice the parsed string, those slices will
+    begin and end with the characters used to bound each group."""
+
+    return tuple([tuple(p) for p in self.parens])
 
   # This lets a GroupParser object behave like an iterator.
   def __iter__(self):
@@ -86,7 +108,7 @@ class GroupParser(object):
           # most recently encountered starting character. Otherwise, complain
           # about unbalanced groups.
           if len(starts)>0 and start_of[self.text[i]]==self.text[starts[-1]]:
-            self.parens.append([starts.pop(),i])
+            self.parens.append([starts.pop(),i+1])
           else:
             if len(starts)>0:
               msg="Unbalanced group at index %d: %r"%(
@@ -103,11 +125,38 @@ class GroupParser(object):
       raise GroupParser.Erorr(
         "No closure found for %r at index %d: %r"%(self.text[i],i,self.text[i:])
       )
+
+    # Sort the groups from left to right by starting position.
+    self.parens.sort(key=lambda p: p[0])
+
     # Return this object to the caller can do more stuff with it.
     return self
 
 if __name__=='__main__':
-  s='This (is a (test).)'
-  p=GroupParser()
+  import argparse
+
+  # Parse our command line.
+  ap=argparse.ArgumentParser()
+  ap.add_argument('-e','--escape',metavar='ESC',nargs='?',const='\\',default=None,help="Ignore grouping characters that are escaped. If an argument is given, it specifies the escape character. Otherwise, the default character is '\\'. If this option is not given, escaping is turned off.")
+  ap.add_argument('args',nargs='*',help="All positionional are combined to create the text to be parsed.")
+  cl=ap.parse_args()
+
+  if cl.args:
+    s=' '.join(cl.args)
+  else:
+    s='Testing (grouping \([regular] express\) text) with {surrounding text}.'
+
+  n=len(s)
+  print ' '+(''.join(['%10d'%(i/10) for i in range(10,n,10)]))
+  print ''.join(['%d'%(i%10) for i in range(n)])
+  print s
+  print
+
+  print "Iterating over parsed groups:"
+  p=GroupParser(escape_char=cl.escape)
   for i,j,t in p(s):
-    print "%d -> %d ==> %r"%(i,j,t)
+    print "  %d:%d ==> %r"%(i,j,t)
+  print
+
+  print 'len() ==> %r'%(len(p),)
+  print 'groups() ==> %r'%(p.groups(),)
