243ef837568a6a77573e77d3a34838a07a35dd78
Jeff Clough <jeff@cloughcottage.com>
2017-06-14 16:00:29 -0400

    Refactored parse_date.py to be better generalized
    I'm using a new class for handling lists of names that should have index
    numbers associated with them. This behaves largely like a dict, but it
    also supports look-up using partial keys. So looking up "Mon" returns
    the same index as "Monday".
    
    class IndexedNames(object)
      Handle the tedium of matching things like day and month names to their
      proper index values, even when given only an abbreviated name to get
      the index value of. Use dictionary syntax to get and set index values
      on an instance of this class.
    
      __contains__(self, key)
      Return true if key is either the name or index of an item stored in
      this object.
    
      __getitem__(self, key)
      If key is a string value, return the index of that string. Otherwise,
      assume key is an index and return the corresponding name.
    
      __init__(self, name_list, start=0, step=1)
      Store an index value, starting at 0 by default, for each name in the
      list. Also precompute all distinct abbreviations for those names,
      remembering each one by the index of the full name.
    
      Every name in the list must be case-insensitively distinct from every
      other name, an no full name may match the beginning of another name.
      Otherwise, a KeyError will be raised.
    
      __repr__(self)
      Return a string that could be passed to eval() to recreate this
      object.
    
      get(self, key, default=None)
      If key is a string value, return the index of that string. Otherwise,
      assume key is an index and return the corresponding name. In either
      case, return the default value if the key is not found.
    
      items(self)
      Return a list of (name, index) tuples that defines this object's
      value, ensuring that the names are returned in the same order as they
      were originally presented to this object. The list returned is
      suitable for constructing a dict.
    
      keys(self)
      Return a list of the names stored in this object in the same order in
      which they were originally presented to it.
    
      values(self)
      Return a list of index values for the names stored in this object,
      ensuring that the index values are in the same order as the names.

--- pylib/parse_date.py
+++ pylib/parse_date.py
@@ -3,95 +3,204 @@
 import re,time
 import datetime as dt
 
-# Day of week data:
-dowl='Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split()
-dowd=dict([
-  (dowl[i].lower(),i) for i in range(len(dowl))
-])
-dowk=dowd.keys()
+import sys # for debugging
 
-# Month data:
-monl='January February March April May June July August September October November December'.split()
-mond=dict([
-  (monl[i].lower(),i) for i in range(len(monl))
-])
-monk=mond.keys()
+class IndexedNames(object):
+  """Handle the tedium of matching things like day and month names to
+  their proper index values, even when given only an abbreviated name to
+  get the index value of. Use dictionary syntax to get and set index
+  values on an instance of this class.
 
-def day_num(s):
-  """Return 0-6 for day of week expressed in s.
-  
-  >>> print day_num('M')
-  0
-  >>> print day_num('Tu')
-  1
-  >>> print day_num('W')
-  2
-  >>> print day_num('Th')
-  3
-  >>> print day_num('F')
+  >>> foo=IndexedNames('Abcd Abef Ghij Klmn Opqr Opqs Opqt'.split(),4,2)
+  >>> print repr(foo)
+  IndexedNames(('Abcd', 'Abef', 'Ghij', 'Klmn', 'Opqr', 'Opqs', 'Opqt'), start=4, step=2)
+  >>> print foo['abc']
   4
-  >>> print day_num('Sa')
-  5
-  >>> print day_num('Su')
+  >>> print foo['abe']
   6
-  >>> print day_num('T')
-  None
-  >>> print day_num('x')
-  None
-  >>> print day_num('monday')
+  >>> foo['totally bogus']
+  Traceback (most recent call last):
+  KeyError: 'totally bogus'
+  >>> foo['a']
+  Traceback (most recent call last):
+  KeyError: 'a'
+  >>> print 'ghij' in foo
+  True
+  >>> print 'ghi' in foo
+  True
+  >>> print 'gh' in foo
+  True
+  >>> print 'g' in foo
+  True
+  >>> print foo[0]
+  Traceback (most recent call last):
+  KeyError: '0'
+  >>> print foo[4]
+  Abcd
+  >>> print foo[8]
+  Ghij
+  >>> foo[99]
+  Traceback (most recent call last):
+  KeyError: '99'
+  >>> foo.items()
+  [('Abcd', 4), ('Abef', 6), ('Ghij', 8), ('Klmn', 10), ('Opqr', 12), ('Opqs', 14), ('Opqt', 16)]
+  >>> foo.keys()
+  ['Abcd', 'Abef', 'Ghij', 'Klmn', 'Opqr', 'Opqs', 'Opqt']
+  >>> foo.values()
+  [4, 6, 8, 10, 12, 14, 16]
+  >>> bar=IndexedNames('abc ABC def ghi'.split())
+  Traceback (most recent call last):
+  KeyError: "Duplicate items in list: 'abc'"
+  >>> bar=IndexedNames('abc ABCD def ghi'.split())
+  Traceback (most recent call last):
+  KeyError: "'abc' is an abbreviation of 'abcd'"
+  >>> dow=IndexedNames('Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split())
+  >>> print dow.get('M')
   0
-  >>> print day_num('mondayx')
-  None
-  """
-
-  s=s.lower()
-  l=len(s)
-  possibles=[d for d in dowk if s==d[:l]]
-  if len(possibles)==1:
-    return dowd[possibles[0]]
-  return None
-
-def month_num(s):
-  """Return 1-12 for the month as expressed in s.
-  
-  >>> print month_num('Ja')
+  >>> print dow.get('Tu')
   1
-  >>> print month_num('F')
+  >>> print dow.get('W')
   2
-  >>> print month_num('Mar')
+  >>> print dow.get('Th')
   3
-  >>> print month_num('Ap')
+  >>> print dow.get('F')
   4
-  >>> print month_num('May')
+  >>> print dow.get('Sa')
   5
-  >>> print month_num('Jun')
+  >>> print dow.get('Su')
   6
-  >>> print month_num('Jul')
-  7
-  >>> print month_num('Au')
-  8
-  >>> print month_num('S')
-  9
-  >>> print month_num('O')
-  10
-  >>> print month_num('N')
-  11
-  >>> print month_num('D')
-  12
-  >>> print month_num('December')
-  12
-  >>> print month_num('Decemberx')
+  >>> print dow.get('T')
   None
-  >>> print month_num('x')
+  >>> print dow.get('x')
   None
+  >>> print dow.get('monday')
+  0
+  >>> print dow.get('mondayx')
+  None
+  
   """
+  
+  def __init__(self,name_list,start=0,step=1):
+    """Store an index value, starting at 0 by default, for each name in
+    the list. Also precompute all distinct abbreviations for those
+    names, remembering each one by the index of the full name.
 
-  s=s.lower()
-  l=len(s)
-  possibles=[d for d in monk if s==d[:l]]
-  if len(possibles)==1:
-    return mond[possibles[0]]+1
-  return None
+    Every name in the list must be case-insensitively distinct from
+    every other name, an no full name may match the beginning of another
+    name. Otherwise, a KeyError will be raised."""
+
+    # Remember our list of name and in what order they were given.
+    self.name_list=tuple(name_list)
+    self.start=int(start)
+    try:
+      self.step=int(step)
+      if self.step==0:
+        raise
+    except:
+      raise ValueError('Illegal step value: %r'%(step,))
+    self.num_dict=dict([
+      (self.name_list[i].lower(),(i,self.start+self.step*i)) for i in range(len(self.name_list))
+    ])
+    # num_dict[key]=(i,start+step*i)
+
+    # Verify that no two names match and that no name is abbreviation for any
+    # other name.
+    klist=self.num_dict.keys()
+    for i in range(len(self.name_list)):
+      for j in range(len(self.name_list)):
+        if i!=j:
+          if self.name_list[i].lower()==self.name_list[j].lower():
+            raise KeyError('Duplicate items in list: %r'%(self.name_list[i].lower()))
+    for i in range(len(klist)):
+      for j in range(len(klist)):
+        if i!=j:
+          if klist[i].startswith(klist[j]):
+            raise KeyError('%r is an abbreviation of %r'%(klist[j],klist[i]))
+
+    # Add decreasingly minimal forms of all keys to dictionary.
+    for name in klist:
+      for i in range(1,len(name)):
+        partial=name[:i]
+        possibles=[s for s in klist if s.startswith(partial)]
+        if len(possibles)==1:
+          val=self.num_dict[name]
+          self.num_dict.update(dict([
+            (name[:j],val) for j in range(i,len(name))
+          ]))
+          break
+
+  def __repr__(self):
+    return '%s(%r, start=%r, step=%r)'%(self.__class__.__name__,self.name_list,self.start,self.step)
+
+  def __contains__(self,key):
+    """Return true if key is either the name or index of an item stored
+    in this object."""
+
+    if isinstance(key,basestring):
+      return key.lower() in self.num_dict
+    try:
+      key=int(key)
+    except:
+      raise KeyError(repr(key))
+    return key in [v[1] for v in self.num_dict.values()]
+
+  def __getitem__(self,key):
+    """If key is a string value, return the index of that string.
+    Otherwise, assume key is an index and return the corresponding
+    name."""
+
+    if isinstance(key,basestring):
+      return self.num_dict[key.lower()][1]
+    try:
+      key=int(key)
+    except:
+      raise KeyError(repr(key))
+    for n in self.name_list:
+      if self.num_dict[n.lower()][1]==key:
+        return n
+    raise KeyError(repr(key))
+
+  def get(self,key,default=None):
+    """If key is a string value, return the index of that string.
+    Otherwise, assume key is an index and return the corresponding
+    name. In either case, return the default value if the key is
+    not found."""
+
+    try:
+      val=self[key]
+    except KeyError:
+      val=default
+    return val
+
+  def items(self):
+    """Return a list of (name, index) tuples that defines this object's
+    value, ensuring that the names are returned in the same order as
+    they were originally presented to this object. The list returned is
+    suitable for constructing a dict."""
+
+    #print >>sys.stderr,'DEBUG: num_dict=%r'%(self.num_dict,)
+
+    return [(n,self.num_dict[n.lower()][1]) for n in self.name_list]
+
+  def keys(self):
+    """Return a list of the names stored in this object in the same
+    order in which they were originally presented to it."""
+
+    return list(self.name_list)
+
+  def values(self):
+    """Return a list of index values for the names stored in this
+    object, ensuring that the index values are in the same order as the
+    names."""
+
+    return [self.num_dict[n.lower()][1] for n in self.name_list]
+
+
+
+# Prepare day-of-week and month-of-year indices, from 0-6 and from 1-12, respectively.
+dow=IndexedNames('Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split())
+moy=IndexedNames('January February March April May June July August September October November December'.split(),1)
+
 
 # [int ](day|week)[s] (ago|before DAY|from DAY|(in the )(past|future))
 # DAY := yesterday|now|today|tomorrow|DOW
@@ -101,7 +210,6 @@ re_relative_date_1=re.compile(
   r'((?P<n>[-+]?\d+)\s+)?'
   r'((?P<units>(day|week)s?)\s+)'
   r'(?P<dir>(ago|(before|from|after)\s+\w+|in\s+the\s+(past|future)))'
-# r'(?P<dir>(ago|from\s+(now|today)|in\s+the\s+(past|future)))'
   r'$'
 )
 
@@ -113,18 +221,17 @@ def parse_date(datestr):
   datestr=datestr.strip().lower()
   lt=time.localtime()
   today=dt.date(*lt[:3])
-  dow=lt[6]
+  this_day=lt[6]
 
-  if s in ('now','today'):
+  if datestr in ('now','today'):
     return today
-  if s=='yesterday':
+  if datestr=='yesterday':
     return today+dt.timedelta(-1)
-  if s=='tomorrow':
+  if datestr=='tomorrow':
     return today+dt.timedelta(1)
 
   m=re_relative_date_1.search(datestr)
   if m:
-    #g=Dict(m.groupdict())
     g=type('',(),m.groupdict())
 
     if g.n:
@@ -141,9 +248,9 @@ def parse_date(datestr):
       g.dir=-1
     elif g.dir[0] in ('before','from','after'):
       dayname=g.dir[-1]
-      day=day_num(dayname)
+      day=dow.get(dayname)
       if day!=None:
-        offset=day-dow
+        offset=day-this_day
         if offset<1:
           offset+=7
       else:
@@ -175,36 +282,41 @@ if __name__=="__main__":
   import doctest
   failed,total=doctest.testmod()
   if failed==0:
-    for s in (
-      'now',
-      'today',
-      'yesterday',
-      'day ago',
-      'day before today',
-      'day after today',
-      'tomorrow',
-      'day from now',
-      'day from today',
-      '1 day ago',
-      '3 days ago',
-      '1 day from now',
-      '2 days from now',
-      '3 days from today',
-      '1 week ago',
-      '-1 week from now',
-      '2 weeks ago',
-      '-2 weeks from today',
-      '1 week from now',
-      '-1 week ago',
-      '2 weeks from today',
-      '-2 weeks in the past',
-      '1 week from monday',
-      '1 week from tue',
-      '1 week from w',
-      '1 week from th',
-      '1 week from fri',
-      '1 week from sat',
-      '1 week from sun',
-      '2 weeks before monday',
-    ):
-      print '%s is %s'%(parse_date(s),s)
+    import sys
+    if len(sys.argv)>1:
+      sample=' '.join(sys.argv[1:])
+      print '%s is %s'%(parse_date(sample),sample)
+    else:
+      for sample in (
+        'now',
+        'today',
+        'yesterday',
+        'day ago',
+        'day before today',
+        'day after today',
+        'tomorrow',
+        'day from now',
+        'day from today',
+        '1 day ago',
+        '3 days ago',
+        '1 day from now',
+        '2 days from now',
+        '3 days from today',
+        '1 week ago',
+        '-1 week from now',
+        '2 weeks ago',
+        '-2 weeks from today',
+        '1 week from now',
+        '-1 week ago',
+        '2 weeks from today',
+        '-2 weeks in the past',
+        '1 week from monday',
+        '1 week from tue',
+        '1 week from w',
+        '1 week from th',
+        '1 week from fri',
+        '1 week from sat',
+        '1 week from sun',
+        '2 weeks before monday',
+      ):
+        print '%s is %s'%(parse_date(sample),sample)
