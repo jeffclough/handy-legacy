c95926301e38feaab7e8beb437ebe6e64735d008
Jeff Clough <jeff@cloughcottage.com>
2020-03-07 04:05:56 -0500

    Improve meta value comparisons
    Most of the internals have changed. The meta value can now end with a
    numeric value that will be treated numericly when comparing one version
    with another.
    
    There's still a problem with DtpVersion, but I'm going to bed. :-)

--- pylib/versioning.py
+++ pylib/versioning.py
@@ -1,15 +1,15 @@
-import re,sys
-
+#!/usr/bin/env python
 """
-
-The Version class is for storing, updating, and comparing semantic
-version data. This versioning module can also be used to update version
-meta data in files (if I'd implemented that feature).
 
 This module is designed to work with any Python version >= 2.7. We can
 a little lower if it's never called as the main module.
 
+This versioning module can also be used to update version meta data in
+existing files (if I'd implemented that feature).
+
 """
+
+import re,sys
 
 # We need this code to run under any version of Python.
 if sys.version_info[0]<3:
@@ -24,17 +24,36 @@ else:
     substitute that should never be used for anything more demanding
     than this module."""
 
-    if type(a) is type(b) and isinstance(a,(int,float,str,tuple,list)):
-      if a<b: return -1
-      elif a>b: return 1
-      else: return 0
-    elif hasattr(a,'__cmp__'):
-      return a.__cmp__(b)
-    else:
-      raise ValueError("Cannot compare %s with %s"%(type(a),type(b)))
+    if type(a) is type(b):
+      if isinstance(a,(int,float,str,tuple,list)):
+        if a<b:
+          return -1
+        elif a>b:
+          return 1
+        else:
+          return 0
+      elif hasattr(a,'__cmp__'):
+        return a.__cmp__(b)
+    raise ValueError("Cannot compare %s with %s"%(type(a),type(b)))
 
 class Version(object):
-  """
+  """The Version class is for storing, updating, and comparing semantic
+  version data.
+
+  >>> Version('1.2.3')._parsed_version
+  (1, 2, 3, None, None)
+  >>> Version('1.2.3.test')._parsed_version
+  (1, 2, 3, 'test', None)
+  >>> Version('1.2.3.123')._parsed_version
+  (1, 2, 3, None, 123)
+  >>> Version('1.2.3.test-123')._parsed_version
+  (1, 2, 3, 'test-', 123)
+  >>> Version('1.2.3.test-123.45')._parsed_version
+  (1, 2, 3, 'test-', 123.45)
+  >>> Version('1.2.3.test.123')._parsed_version
+  (1, 2, 3, 'test.', 123)
+  >>> Version('1.2.3.test.123.45')._parsed_version
+  (1, 2, 3, 'test.', 123.45)
   >>> a=Version()
   >>> str(a)
   '0.0.0'
@@ -43,6 +62,8 @@ class Version(object):
   >>> a=Version((1,2,3))
   >>> a._version
   [1, 2, 3, '']
+  >>> a._parsed_version
+  (1, 2, 3, None, None)
   >>> a.major
   1
   >>> a.minor
@@ -63,10 +84,14 @@ class Version(object):
   'beta'
   >>> a._version
   [1, 2, 3, 'beta']
+  >>> a._parsed_version
+  (1, 2, 3, 'beta', None)
   >>> str(a)
   '1.2.3.beta'
   >>> str(b)
   '1.2.3'
+  >>> b._parsed_version
+  (1, 2, 3, None, None)
   >>> a<b
   True
   >>> a<=b
@@ -81,43 +106,8 @@ class Version(object):
   False
   """
 
-  _parser=re.compile(r'(?P<major>\d+)(\.(?P<minor>\d+)(\.(?P<patch>\d+)(\.(?P<meta>.+))?)?)?$')
-
-  def _meta_cmp(self,other):
-    """Compare the metadata portions of self and other, returning ...
-    
-        -1 if self.meta<other.meta,
-         0 if self.meta==other.meta, and
-         1 if self.meta>other.meta.
-     
-    This comparison differs from an ordinary string comparison in that
-    the empty string is greater than any non-empty string. So "1.2.3" >
-    "1.2.3.something".
-    
-    >>> a=Version('1.2.3')
-    >>> b=Version('2.3.4')
-    >>> a._meta_cmp(b)
-    0
-    >>> a.meta='abc'
-    >>> b.meta='abc'
-    >>> a._meta_cmp(b)
-    0
-    >>> b.meta=''
-    >>> a._meta_cmp(b)
-    -1
-    >>> b._meta_cmp(a)
-    1
-    >>> b.meta='def'
-    >>> a._meta_cmp(b)
-    -1
-    >>> b._meta_cmp(a)
-    1
-    """
-
-    diff=cmp(self.meta,other.meta)
-    if bool(self.meta)!=bool(other.meta):
-      diff=-diff
-    return diff
+  __version_parser=re.compile(r'(?P<major>\d+)(\.(?P<minor>\d+)(\.(?P<patch>\d+)(\.(?P<meta>.+))?)?)?$')
+  __version_meta_parser=re.compile(r'(?P<s>.*?)(?P<n>\d+(\.\d+)?)?$')
 
   def __init__(self,arg=None):
     """The string form of a version constists if up to 3 numbers
@@ -128,20 +118,21 @@ class Version(object):
         1.2
         1.2.3
         1.2.3.beta
+        1.2.3.beta-3
 
     The numeric parts are, from left to right, called the major, minor,
     and patch numbers. The metadata can be any string value.
-    
+
     Initialize this Version instance from a version string, a tuple,
     list, or another Version instance. If arg is None (the default), our
     value defaults to 0.0.0.
     """
 
-    if arg is None:
+    if not arg:
       self._version=[0,0,0,'']
     elif isinstance(arg,stringtype):
       # Parse this string.
-      m=Version._parser.match(arg)
+      m=Version.__version_parser.match(arg)
       if not m:
         raise ValueError("Invalid initializer ('%s') for Version instance."%arg)
       d=m.groupdict()
@@ -160,7 +151,9 @@ class Version(object):
         for i in range(3):
           vals[i]=int(vals[i])
       except:
-        raise ValueError("Invalid initializer (%r) for Version with non-numeric value in positions %d."%(arg,i+1))
+        raise ValueError("Invalid initializer (%r) for %s with non-numeric value at index %d."%(
+          arg,self.__cls__.__name__,i
+        ))
       if len(vals)<4:
         vals.append('')
       elif len(vals)>4:
@@ -171,42 +164,41 @@ class Version(object):
       self._version=list(arg._version)
     else:
       # We don't know what to do with this initializer argument.
-      raise ValueError("Invalid initializer type (%s) for Version instance."%type(arg))
+      raise ValueError("Invalid initializer type (%s) for %s."%(type(arg),self.__cls__.__name__))
+    self.tuplize()
+
+  def tuplize(self):
+    """Set this Version instance's _parsed_version attribute. This must
+    be called every time our _version attribute changes."""
+
+    self._parsed_version=tuple([
+      self.major,
+      self.minor,
+      self.patch
+    ]+list(self.metaParser(self.meta)))
 
   def __cmp__(self,other):
-    """Compare this object's value with that of the other object,
-    returning -1 if self<other, 0 if self==other, and 1 if self>other.
-    The three numeric components are compared numerically. The metadata
-    component is compared using the meta_comp() class method.
-    
-    The _meta_cmp() class method may be overridden in a subclass if you
-    want to provide your own logic for that. For example if
-    dev<test<prod, or if numeric metadata should not be compared as
-    string values, you'll want to provide your own method for handling
-    that.
-    
-    See _meta_cmp()'s documentation for a description of Version's
-    implementation."""
+    "Return -1 if self<other, 0 if self==other, or 1 if self>other."
 
     a=self._version[:3]
     b=other._version[:3]
     if a!=b:
       diff=cmp(a,b)
     else:
-      diff=self._meta_cmp(other)
+      diff=self.meta_compare(other)
     return diff
 
-  def __eq__(self,other): return self._version==other._version
-  def __ne__(self,other): return self._version!=other._version
-  #def __lt__(self,other): return self.__cmp__(other)<0
-  #def __le__(self,other): return self.__cmp__(other)<=0
-  #def __gt__(self,other): return self.__cmp__(other)>0
-  #def __ge__(self,other): return self.__cmp__(other)>=0
-  def __lt__(self,other): return cmp(self,other)<0
-  def __le__(self,other): return cmp(self,other)<=0
-  def __gt__(self,other): return cmp(self,other)>0
-  def __ge__(self,other): return cmp(self,other)>=0
- 
+  def meta_compare(self,other):
+    "Just like __cmp__, but looks only at the meta values."
+
+    diff=cmp(self.meta,other.meta)
+    a_empty=not all([x==None for x in self.meta])
+    b_empty=not all([x==None for x in other.meta])
+    if a_empty!=b_empty:
+      diff=-diff
+    return diff
+    
+
   def __getitem__(self,i):
     "Return the ith elelement of our value."
 
@@ -216,6 +208,7 @@ class Version(object):
     "Set the ith element of our value."
 
     self._version[i]=val
+    self.tuplize()
 
   def __str__(self):
     "Return a string version of this Version object's value."
@@ -237,35 +230,114 @@ class Version(object):
       yield part
 
   @property
-  def major(self): return self[0]
+  def major(self):
+    return self._version[0]
 
   @major.setter
-  def major(self,val): self[0]=int(val)
+  def major(self,n):
+    if not isinstance(n,int):
+      n=int(n)
+    self._version[0]=n
+    self.tuplize()
 
   @property
-  def minor(self): return self[1]
+  def minor(self):
+    return self._version[1]
 
   @minor.setter
-  def minor(self,val): self[1]=int(val)
+  def minor(self,n):
+    if not isinstance(n,int):
+      n=int(n)
+    self._version[1]=n
+    self.tuplize()
 
   @property
-  def patch(self): return self[2]
+  def patch(self):
+    return self._version[2]
 
   @patch.setter
-  def patch(self,val): self[2]=int(val)
+  def patch(self,n):
+    if not isinstance(n,int):
+      n=int(n)
+    self._version[2]=n
+    self.tuplize()
 
   @property
-  def meta(self): return self[3]
+  def meta(self):
+    return self._version[3]
 
   @meta.setter
-  def meta(self,val): self[3]=val
+  def meta(self,val):
+    if not isinstance(val,stringtype):
+      val=str(val)
+    self._version[3]=val
+    self.tuplize()
+
+  @classmethod
+  def metaParser(cls,val):
+    """Return any (string,number) tuple that can be parsed from our meta
+    value. The number may be an int or float value. If meta doesn't
+    begin with a non-numeric string, the tuple's first element will be
+    None. If meta doesn't end with a number, the tuple's second element
+    will be None.
+
+    >>> Version.metaParser('')
+    (None, None)
+    >>> Version.metaParser('test')
+    ('test', None)
+    >>> Version.metaParser('123')
+    (None, 123)
+    >>> Version.metaParser('123.45')
+    (None, 123.45)
+    >>> Version.metaParser('test-123.45')
+    ('test-', 123.45)
+    """
+
+    if not isinstance(val,stringtype):
+      val=str(val)
+    m=cls.__version_meta_parser.match(val)
+    s=n=None
+    if m:
+      d=m.groupdict()
+      s=(None,d['s'])[bool(d['s'])]
+      n=(None,d['n'])[bool(d['n'])]
+      if n:
+        try:
+          n=int(n)
+        except:
+          try:
+            n=float(n)
+          except:
+            n=None
+            s=val
+      else:
+        n=None
+    return (s,n)
 
 python_version=Version(sys.version_info[:4])
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 class DtpVersion(Version):
-  """
+  """DtpVersion is just like Version, except that it recognizes the
+  standard values "dev", "test", "alpha", "beta", and "prod" in it's meta component. For
+  purposes of comparing DtpVersion objects, each value is greater than
+  the one before it (in the order shown above).
+
+  A meta value of "" is greater than "prod", but any other non-standard
+  value is less than "dev" and compares any with other non-standard
+  value alphabetically.
+
+  Whatever the meta value is, any standard value may be followed by an
+  arbitrary string that only participates in comparison operations if
+  the standard part of each DtpVersion's meta value is the same. If the
+  standard part of meta's value begins with a '.', the remainder of the
+  value will be interpreted numerically (if possible) for comparison purposes.
+  Otherwise, it will be interpreted as a string. So "1.2.3.prod.11" is
+  greater than "1.2.3.prod.2", and "1.2.3.beta-dog" is greater than
+  "1.2.3.beta-chimp", but remember that "1.2.3.test-anything" is
+  greater than "1.2.3.dev-something"
+
   >>> a=DtpVersion('4.5.6')
   >>> str(a)
   '4.5.6'
@@ -307,39 +379,45 @@ class DtpVersion(Version):
   True
   >>> a>x
   True
+  >>> DtpVersion("1.2.3.prod.11")>DtpVersion("1.2.3.prod.2")
+  True
+  >>> DtpVersion("1.2.3.beta-dog")>DtpVersion("1.2.3.beta-chimp")
+  True
+  >>> DtpVersion("1.2.3.test-anything")>DtpVersion("1.2.3.dev-something")
+  True
   """
 
   env=dict(
     dev=1,
     test=2,
-    prod=3
+    alpha=3,
+    beta=4,
+    prod=5
   )
 
   def __init__(self,arg):
     # Leave everything to the superclass.
     super(DtpVersion,self).__init__(arg)
 
-  def _meta_cmp(self,other):
-    """Compare the metadata portion of self and other, returning ...
-        -1 if self.meta<other.meta,
-         0 if self.meta==other.meta, and
-         1 if self.meta>other.meta.
-     
-    This implementation uses dev<test<prod ordering, and everything else
-    is less than dev, and an empty metadata string is greater than
-    anything else."""
+  @classmethod
+  def metaParser(cls,val):
+    """Return an (env_rank,(string,number)) tuple from the given meta
+    value."""
 
-    if '' in (self.meta,other.meta):
-      diff=-cmp(self.meta,other.meta)
+    if not val:
+      # An empty meta value is greater than any standard value.
+      rank,suffix=DtpVersion.env['prod']+1,''
     else:
-      i=DtpVersion.env.get(self.meta,0)
-      j=DtpVersion.env.get(other.meta,0)
-      diff=i-j
-    if diff<0:
-      diff=-1
-    elif diff>0:
-      diff=1
-    return diff
+      # Find any standard value and its sufix.
+      for e in DtpVersion.env:
+        if val.startswith(e):
+          l=len(e)
+          rank,suffix=DtpVersion.env[val[:l]],val[l:]
+      else:
+        # Whatever this is is valued less than "dev", and the whole string is the suffix.
+        rank,suffix=0,val
+    suffix=super(DtpVersion,cls).metaParser(suffix)
+    return (rank,suffix)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
