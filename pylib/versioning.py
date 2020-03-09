8d0cf33f69471d86fbb4baa441a3f80f48f10e02
Jeff Clough <jeff@cloughcottage.com>
2020-03-08 21:44:53 -0400

    Finish debugging versioning.py
    I've renamed the DtpVersion class to RankedVersion and made the ranks
    something the caller can set for himself. The next step (inviting
    further debugging) is to figure out a good way to read and write version
    data in source files.
    
    ---
    
    The Version class is for storing, updating, and comparing semantic
    version data. This is a very simple implementation, but it does allow
    version comparisons to be numeric, so 1.5 < 1.10.
    
    The meta value defaults to an empty string, but there's a fair amount of
    versatility there too. Any numeric suffix is treated numerically so that
    1.2.3.beta-5 < 1.2.3.beta-10, but the string portion is still compared
    alphabetically, so 1.2.3.test-5 > 1.2.3.prod-5.
    
    There's also the problem that 1.2.3 < 1.2.3.prod, but see the
    RankedVersion for a solution to that.
    
    ---
    
    RankedVersion is just like Version, except that it recognizes the rank
    values "dev", "test", and "prod" (by default) in its meta component. For
    purposes of comparing RankedVersion objects, each value is greater than
    the one before it (in the order shown above).
    
    A meta value of "" is greater than "prod", but any other non-ranked
    value is less than "dev" and compares any with other non-ranked value
    alphabetically.
    
    Whatever the meta value is, any ranked value may be followed by an
    arbitrary string that only participates in comparison operations if the
    ranked part of each RankedVersion's meta value is the same. This may be
    suffixed with a numeric value, which if present, will participate in
    comparison operation according to its numeric value. So "1.2.3.prod.11"
    is greater than "1.2.3.prod.2", and "1.2.3.beta-dog" is greater than
    "1.2.3.beta-chimp", but remember that "1.2.3.test-anything" is greater
    than "1.2.3.dev-something" That's what ranking is all about.

--- pylib/versioning.py
+++ pylib/versioning.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 """
-
-This module is designed to work with any Python version >= 2.7. We can
-a little lower if it's never called as the main module.
+This module is designed to work with any Python version >= 2.7
+(including 3.*). We can go a little lower if it's never called as the
+main module.
 
 This versioning module can also be used to update version meta data in
-existing files (if I'd implemented that feature).
-
+existing files (once I implement that feature).
 """
 
 import re,sys
@@ -38,7 +37,16 @@ else:
 
 class Version(object):
   """The Version class is for storing, updating, and comparing semantic
-  version data.
+  version data. This is a very simple implementation, but it does allow
+  version comparisons to be numeric, so 1.5 < 1.10.
+
+  The meta value defaults to an empty string, but there's a fair amount
+  of versatility there too. Any numeric suffix is treated numerically so
+  that 1.2.3.beta-5 < 1.2.3.beta-10, but the string portion is still
+  compared alphabetically, so 1.2.3.test-5 > 1.2.3.prod-5.
+
+  There's also the problem that 1.2.3 < 1.2.3.prod, but see the
+  RankedVersion for a solution to that.
 
   >>> Version('1.2.3')._parsed_version
   (1, 2, 3, '', 0)
@@ -75,6 +83,8 @@ class Version(object):
   >>> a[3]
   ''
   >>> b=Version(a)
+  >>> b._parsed_version
+  (1, 2, 3, '', 0)
   >>> a==b
   True
   >>> a.meta='beta'
@@ -93,17 +103,17 @@ class Version(object):
   >>> b._parsed_version
   (1, 2, 3, '', 0)
   >>> a<b
-  True
+  False
   >>> a<=b
-  True
+  False
   >>> a==b
   False
   >>> a!=b
   True
   >>> a>=b
-  False
+  True
   >>> a>b
-  False
+  True
   """
 
   __version_parser=re.compile(r'(?P<major>\d+)(\.(?P<minor>\d+)(\.(?P<patch>\d+)(\.(?P<meta>.+))?)?)?$')
@@ -125,8 +135,7 @@ class Version(object):
 
     Initialize this Version instance from a version string, a tuple,
     list, or another Version instance. If arg is None (the default), our
-    value defaults to 0.0.0.
-    """
+    value defaults to 0.0.0."""
 
     if not arg:
       self._version=[0,0,0,'']
@@ -177,27 +186,34 @@ class Version(object):
       self.patch
     ]+list(self.metaParser(self.meta)))
 
+  # Python 2 uses __cmp__() for comparison operations.
   def __cmp__(self,other):
     "Return -1 if self<other, 0 if self==other, or 1 if self>other."
 
-    a=self._version[:3]
-    b=other._version[:3]
-    if a!=b:
-      diff=cmp(a,b)
-    else:
+    diff=cmp(self._parsed_version[:3],other._parsed_version[:3])
+    if diff==0:
       diff=self.meta_compare(other)
     return diff
 
   def meta_compare(self,other):
     "Just like __cmp__, but looks only at the meta values."
 
-    diff=cmp(self.meta,other.meta)
-    a_empty=not all([x==None for x in self.meta])
-    b_empty=not all([x==None for x in other.meta])
-    if a_empty!=b_empty:
-      diff=-diff
+    a_meta=self._parsed_version[3:]
+    b_meta=other._parsed_version[3:]
+    diff=cmp(a_meta,b_meta)
+    #a_empty=not all([x==None for x in a_meta])
+    #b_empty=not all([x==None for x in b_meta])
+    #if a_empty!=b_empty:
+    #  diff=-diff
     return diff
     
+  # These overrides are required for Python 3.
+  def __eq__(self,other): return self.__cmp__(other)==0
+  def __ne__(self,other): return self.__cmp__(other)!=0
+  def __lt__(self,other): return self.__cmp__(other)<0
+  def __le__(self,other): return self.__cmp__(other)<=0
+  def __ge__(self,other): return self.__cmp__(other)>=0
+  def __gt__(self,other): return self.__cmp__(other)>0
 
   def __getitem__(self,i):
     "Return the ith elelement of our value."
@@ -215,13 +231,13 @@ class Version(object):
 
     s='.'.join([str(part) for part in self._version[:3]])
     if self._version[3]:
-      s+='.%s'%self._version[3]
+      s+='.%s'%(self._version[3],)
     return s
 
   def __repr__(self):
     "Return a evaluatable string that would recreate this object."
 
-    return "Version(%r)"%str(self)
+    return "Version(%r)"%(str(self),)
 
   def __iter__(self):
     "Allow iteration to support coercion to tuple and list types."
@@ -268,7 +284,9 @@ class Version(object):
 
   @meta.setter
   def meta(self,val):
-    if not isinstance(val,stringtype):
+    if val==None:
+      val=''
+    elif not isinstance(val,stringtype):
       val=str(val)
     self._version[3]=val
     self.tuplize()
@@ -318,94 +336,132 @@ python_version=Version(sys.version_info[:4])
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-class DtpVersion(Version):
-  """DtpVersion is just like Version, except that it recognizes the
-  standard values "dev", "test", "alpha", "beta", and "prod" in it's meta component. For
-  purposes of comparing DtpVersion objects, each value is greater than
-  the one before it (in the order shown above).
+class RankedVersion(Version):
+  """RankedVersion is just like Version, except that it recognizes the
+  rank values "dev", "test", and "prod" (by default) in its meta
+  component. For purposes of comparing RankedVersion objects, each value
+  is greater than the one before it (in the order shown above).
 
-  A meta value of "" is greater than "prod", but any other non-standard
-  value is less than "dev" and compares any with other non-standard
-  value alphabetically.
+  A meta value of "" is greater than "prod", but any other non-ranked
+  value is less than "dev" and compares any with other non-ranked value
+  alphabetically.
 
-  Whatever the meta value is, any standard value may be followed by an
+  Whatever the meta value is, any ranked value may be followed by an
   arbitrary string that only participates in comparison operations if
-  the standard part of each DtpVersion's meta value is the same. If the
-  standard part of meta's value begins with a '.', the remainder of the
-  value will be interpreted numerically (if possible) for comparison purposes.
-  Otherwise, it will be interpreted as a string. So "1.2.3.prod.11" is
-  greater than "1.2.3.prod.2", and "1.2.3.beta-dog" is greater than
-  "1.2.3.beta-chimp", but remember that "1.2.3.test-anything" is
-  greater than "1.2.3.dev-something"
+  the ranked part of each RankedVersion's meta value is the same. This
+  may be suffixed with a numeric value, which if present, will
+  participate in comparison operation according to its numeric value. So
+  "1.2.3.prod.11" is greater than "1.2.3.prod.2", and "1.2.3.beta-dog"
+  is greater than "1.2.3.beta-chimp", but remember that
+  "1.2.3.test-anything" is greater than "1.2.3.dev-something" That's
+  what ranking is all about.
 
-  >>> a=DtpVersion('4.5.6')
-  >>> str(a)
-  '4.5.6'
+  >>> sorted(RankedVersion._ranks.items())
+  [('dev', 0), ('prod', 2), ('test', 1)]
+
+  >>> a=RankedVersion('1.2.3')
   >>> a._parsed_version
-  (4, 5, 6, 6, '', 0)
-  >>> dev=DtpVersion(a)
-  >>> dev.meta='dev'
-  >>> str(dev)
-  '4.5.6.dev'
-  >>> dev._parsed_version
-  (4, 5, 6, 1, '', 0)
-  >>> test=DtpVersion(a)
-  >>> test.meta='test'
-  >>> str(test)
-  '4.5.6.test'
-  >>> test._parsed_version
-  (4, 5, 6, 2, '', 0)
-  >>> prod=DtpVersion(a)
-  >>> prod.meta='prod'
-  >>> str(prod)
-  '4.5.6.prod'
-  >>> prod._parsed_version
-  (4, 5, 6, 5, '', 0)
-  >>> dev==test
-  False
-  >>> test==prod
-  False
-  >>> prod==dev
-  False
-  >>> a==dev
-  False
-  >>> dev<test
+  (1, 2, 3, 3, '', 0)
+  >>> str(a)
+  '1.2.3'
+
+  >>> b=RankedVersion(a)
+  >>> id(a)!=id(b)
   True
-  >>> dev<prod
+  >>> b._parsed_version
+  (1, 2, 3, 3, '', 0)
+  >>> str(b)
+  '1.2.3'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (True, False, False, True, True, False)
+
+  >>> b.patch=15
+  >>> b._parsed_version
+  (1, 2, 15, 3, '', 0)
+  >>> str(b)
+  '1.2.15'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (False, True, True, True, False, False)
+
+  >>> b.patch='3'
+  >>> b._parsed_version
+  (1, 2, 3, 3, '', 0)
+
+  >>> b.meta='prod'
+  >>> b._parsed_version
+  (1, 2, 3, 2, '', 0)
+  >>> str(b)
+  '1.2.3.prod'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (False, True, False, False, True, True)
+
+  >>> a.meta='test'
+  >>> a._parsed_version
+  (1, 2, 3, 1, '', 0)
+  >>> str(a)
+  '1.2.3.test'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (False, True, True, True, False, False)
+
+  >>> b.meta='dev'
+  >>> b._parsed_version
+  (1, 2, 3, 0, '', 0)
+  >>> str(b)
+  '1.2.3.dev'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (False, True, False, False, True, True)
+
+  >>> a.meta=''
+  >>> a._parsed_version
+  (1, 2, 3, 3, '', 0)
+  >>> str(a)
+  '1.2.3'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (False, True, False, False, True, True)
+
+  >>> a.meta='bogus'
+  >>> a._parsed_version
+  (1, 2, 3, -1, 'bogus', 0)
+  >>> str(a)
+  '1.2.3.bogus'
+  >>> a==b, a!=b, a<b, a<=b, a>=b, a>b
+  (False, True, True, True, False, False)
+
+  >>> RankedVersion("1.2.3.prod.11")>RankedVersion("1.2.3.prod.2")
   True
-  >>> test<prod
+  >>> RankedVersion("1.2.3.beta-dog")>RankedVersion("1.2.3.beta-chimp")
   True
-  >>> prod<a
-  True
-  >>> x=DtpVersion(a)
-  >>> x.meta='x'
-  >>> str(x)
-  '4.5.6.x'
-  >>> a>x
-  True
-  >>> prod>x
-  True
-  >>> a>x
-  True
-  >>> DtpVersion("1.2.3.prod.11")>DtpVersion("1.2.3.prod.2")
-  True
-  >>> DtpVersion("1.2.3.beta-dog")>DtpVersion("1.2.3.beta-chimp")
-  True
-  >>> DtpVersion("1.2.3.test-anything")>DtpVersion("1.2.3.dev-something")
+  >>> RankedVersion("1.2.3.test-anything")>RankedVersion("1.2.3.dev-something")
   True
   """
 
-  env=dict(
-    dev=1,
-    test=2,
-    alpha=3,
-    beta=4,
-    prod=5
-  )
+  # DO NOT MODIFY _ranks DIRECTLY. Use RankedVersion.setRanks() instead. You
+  # have been warned.
+  _ranks={}
 
   def __init__(self,arg):
     # Leave everything the superclass.
-    super(DtpVersion,self).__init__(arg)
+    super(RankedVersion,self).__init__(arg)
+
+  @classmethod
+  def setRanks(cls,*ranks):
+    """Assign the rank names, in order, to this class. The default ranks
+    are:
+        dev
+        test
+        prod
+
+    If you'd like other, you can set them up by calling this class
+    method. For example:
+
+        RankedVersion.setRanks('dev','test','canditate','final')
+
+    Use whatever ranking terminology you prefer, but be sure call this
+    method BEFORE instantiating any RankedVersion objects."""
+
+    n=len(ranks)
+    cls._ranks=dict(zip([str(r) for r in ranks],range(n)))
+    cls.unranked=n
 
   @classmethod
   def metaParser(cls,val):
@@ -416,33 +472,30 @@ class DtpVersion(Version):
     Version's tuplize() method during construction and whenever this
     instance's data changes."""
 
-    log.debug('%s.metaParser(%r)'%(cls.__name__,val))
     if not val:
-      # An empty meta value is greater than any standard value.
-      rank,suffix=DtpVersion.env['prod']+1,''
+      # An empty meta value is greater than any ranked value.
+      rank,suffix=cls.unranked,''
     else:
       # Find any standard value and its sufix.
-      for e in DtpVersion.env:
+      for e in RankedVersion._ranks:
         if val.startswith(e):
           l=len(e)
           # Get the numeric rank and any remainder of the meta value.
-          rank,suffix=DtpVersion.env[val[:l]],val[l:]
-          log.debug('rank=%r, suffix=%r'%(rank,suffix))
+          rank,suffix=RankedVersion._ranks[val[:l]],val[l:]
           break
       else:
-        # Whatever this is is valued less than "dev", and the whole string is the suffix.
-        rank,suffix=0,val
-    suffix=super(DtpVersion,cls).metaParser(suffix)
+        # Whatever this is is valued less than our least rank, and the whole string is the suffix.
+        rank,suffix=-1,val
+    suffix=super(RankedVersion,cls).metaParser(suffix)
     meta=tuple([rank]+list(suffix))
-    log.debug('--> %r'%(meta,))
     return meta
+
+# Set up our default rank names (in order).
+RankedVersion.setRanks('dev','test','prod')
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if __name__=='__main__':
-  import loggy
-  log=loggy.get_logger(facility='d',level='debug')
-  log.info('starting --------')
 
   try:
     import argparse
@@ -454,7 +507,6 @@ if __name__=='__main__':
   opt=ap.parse_args()
 
   if opt.test:
-    log.info('running tests')
     import doctest
     failed,_=doctest.testmod()
     if failed==0:
