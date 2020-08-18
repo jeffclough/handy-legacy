b15a376ff31e44a6682000b3b277ec6ac36c45ac
Jeff Clough <jeff@cloughcottage.com>
2020-08-17 23:11:10 -0400

    GREATLY improve the debug.py module
    - Simplified how the DebugChannel's write() method finds the line that
      called this module. The new method requires that the global __loader__
      variable exist, which means we can't simply run
    
          python debug.py
    
        to run our test code. We have to run
    
          python -m debug
    
    - Took my own advice, ditching DebugChannelBasename in favor of letting
      the caller use DebugChannel's fmt attribute to use either {pathname}
      (which is the full path to the calling module) or {basename} (only the
      basename of the calling module). This is just so much simpler. I also
      made basename the default:
    
          '{label}: {basename}({line}): {indent}{message}\n'
    
      Note that there's no {filename} value available anymore. Choose either
      {pathname} or {basename}.
    
    - GREATLY improved write() to handle messages of one-line strings,
      multi-line strings, lists, tuples, and dictionaries.
    
    - Added a writelines() method, which is just a simple wrapper around
      write(). Its only purpose is to support code that expects to find a
      writelines() method.
    
    - Added a line_iter(s) function. This is a generator that yields each
      line of a multi-line string in place, without having to create a list
      containing those lines. This means we can debug a very long, multi-
      line string value without having to copy the whole thing into new
      memory. This is called from the DebugChannel's write() method, but
      it's available to callers if they want to be able to step through line
      in multi-line strings. It's a handy thing.
    
    - Added to the test code so we can test all these new features.

--- pylib/debug.py
+++ pylib/debug.py
@@ -1,5 +1,22 @@
 import inspect,os,sys
 
+def line_iter(s):
+  """This iterator facilitates stepping through each line of a multi-
+  line string in place, without having to create a list containing those
+  lines."""
+
+  i=0
+  n=len(s)
+  while i<n:
+    j=s.find(os.linesep,i)
+    if j<0:
+      yield s[i:] # Yield the rest of this string.
+      j=n
+    else:
+      yield s[i:j] # Yield the next line in this string.
+      j+=1
+    i=j
+
 class DebugChannel(object):
   """Objects of this class are really useful for debugging, and this is
   even more powerful when combined with loggy.LogStream to write all
@@ -12,12 +29,11 @@ class DebugChannel(object):
       from loggy import LogStream
 
       d=DebugChannel(True,LogStream(facility='user'))
-      d.setFormat('{filename}({line}): {indent}{message}')
       d('Testing')
 
-  The output in /var/log/user.log might look like this
+  The output in /var/log/user.log might look like this:
 
-      Aug 16 22:58:16 pi4 x[18478] D: /home/jclough/my/bin/x(8): Testing
+      Aug 16 22:58:16 pi4 x[18478] D: x(8): Testing
 
   What I really like about this is that the source filename and line
   number are included in the log output. The "d('Testing')" call is on
@@ -49,20 +65,29 @@ class DebugChannel(object):
     self.stream=stream
     self.enabled=enabled
 
-    self.fmt='{label}: {filename}({line}): {indent}{message}\n'
+    self.fmt='{label}: {basename}({line}): {indent}{message}\n'
     self.indlev=0
     self.label=label
     self.indstr='  '
-    self.ignore_modules=set(['loggy.py'])
-
-  @staticmethod
-  def filenamer(filename):
-    return filename
+    # So we don't report anything in this module as the caller ...
+    self.ignore_modules=set([__loader__.filename])
 
   def __bool__(self):
-    "Return the Enabled state of this DebugChannel object."
+    """Return the Enabled state of this DebugChannel object. It is
+    somtimes necessary to logically test whether our code is in debug
+    mode at runtime, and this method makes that very simple.
 
-    return self.enabled
+        d=DebugChannel(opt.debug)
+        .
+        .
+        .
+        if d:
+          d("Getting diagnostics ...")
+          diagnostics=some_expensive_data()
+          d.writelines(diagnostics)
+    """
+
+    return bool(self.enabled)
 
   def enable(self,state=True):
     """Allow this DebugChannel object to write messages if state is
@@ -75,11 +100,12 @@ class DebugChannel(object):
   def setFormat(self,fmt):
     """Set the format of our debug statements. The format defaults to:
 
-        '{label}: {filename}({line}): {indent}{message}\\n'
+        '{label}: {basename}({line}): {indent}{message}\\n'
 
     Fields:
       {label}    printed before the colon (default: 'DEBUG')
-      {filename} name of the calling source file
+      {pathname} full path of the calling source file
+      {basename} base name of the calling source file
       {line}     number of the calling line of code in its source file
       {indent}   indention string multiplied by the indention level
       {message}  the message to be written
@@ -114,7 +140,7 @@ class DebugChannel(object):
     our current format. In any case, return this DebugChannel instance
     so that, for example, things like this will work:
 
-        debug=DebugChannel(Boolean(True))
+        debug=DebugChannel(opt.debug)
         debug('Testing')
 
         def func(arg):
@@ -124,57 +150,101 @@ class DebugChannel(object):
           debug.indent(-1).write("Leaving func()")
 
     This lets the caller decide whether to change indenture before or
-    after the message is written."""
+    after the message is written.
+    
+    If message is a single string containing no line endings, that
+    single value will be outout.
+
+    if message contains at least one newline (the value of os.linesep),
+    each line is output on a debug line of its own.
+    
+    If message is a list or tuple, each item in that sequence will be
+    output on its own line.
+    
+    If message is a dictionary, each key/value pair is written out as
+    
+        key: value
+        
+    to its own log line. The keys are sorted in ascending order."""
 
     if self.enabled:
-      filename,line=None,None
+      # Figure out where the caller called us from.
+      pathname,basename,line=None,None,None
       stack=inspect.stack()
       max_frame=len(stack)-1
-      #sys.stderr.write("stack length=%d\n"%(max_frame+1,))
-      #sys.stderr.write("%s\n"%('\n'.join(["  %d: %r"%(i,stack[i]) for i in range(max_frame+1)])))
-      if len(stack)>=2:
-        if stack[0][1]==stack[1][1] and stack[1][3]=='__call__':
-          # write() was called from our own __call__ method.
-          i=2
-        else:
-          i=1
-        if self.ignore_modules:
-          while i<max_frame and stack[i] in self.ignore_modules:
-            i+=1
-        filename=self.filenamer(stack[i][1])
-        line=stack[i][2]
+      i=0
+      while i<max_frame and stack[i][1] in self.ignore_modules:
+        i+=1
+      pathname,line=stack[i][1:3]
+      basename=os.path.basename(pathname)
       indent=self.indstr*self.indlev
       label=self.label
-      self.stream.write(self.fmt.format(**locals()))
+
+      # Format our message and write it to the debug stream.
+      if isinstance(message,(list,tuple)):
+        messages=message
+        for message in messages:
+          self.stream.write(self.fmt.format(**locals()))
+      elif isinstance(message,dict):
+        messages=message
+        for k in sorted(messages.keys()):
+          message='%s: %s'%(k,messages[k])
+          self.stream.write(self.fmt.format(**locals()))
+      elif isinstance(message,basestring) and os.linesep in message:
+        messages=message
+        for message in line_iter(messages):
+          self.stream.write(self.fmt.format(**locals()))
+      else:
+        self.stream.write(self.fmt.format(**locals()))
+
+    # Let the caller call other methods by using our return value.
     return self
+
+  def writelines(self,seq):
+    """Just a wrapper around write(), since that method handles
+    sequences (and other things) just fine. writelines() is only
+    provided for compatibility with code that expects it to be
+    supported."""
+
+    return self.write(seq)
 
   def __call__(self,message):
-    "Just a wrapper for the write() method."
+    """Just a wrapper for the write() method. Message can be a single-
+    line string, multi-line string, list, tuple, or dict. See write()
+    for details."""
 
-    self.write(message)
-    return self
-
-class DebugChannelBasename(DebugChannel):
-  def __init__(self,enabled=False,stream=sys.stderr,label='DEBUG'):
-    super(self.__class__,self).__init__(enabled,stream,label)
-
-  @staticmethod
-  def filenamer(filename):
-    return os.path.basename(filename)
+    return self.write(message)
 
 if __name__=='__main__':
+  assert '__loader__' in globals(),"To run this module stand-along, use 'python -m debug'."
   # Create our DebugChannel object that is switched on.
   d=DebugChannel(True)
+  d('BASIC OUTPUT ...').indent()
   d('Message 1')
   d('Message 2')
+  d.undent()('INDENTED OUTPUT ...').indent().indstr='| '
   d('indlev=%r'%(d.indlev,)).indent(1)
-  d('indlev=%r'%(d.indlev,)).indent(1).indstr='| '
   d('indlev=%r'%(d.indlev,)).indent(1)
+  d('indlev=%r'%(d.indlev,)).indent(1)
+  d('indlev=%r'%(d.indlev,))
+  d('same level')
   d.indent(-1)('indlev=%r'%(d.indlev,))
   d.indent(-1)('indlev=%r'%(d.indlev,))
   d.indent(-1)('indlev=%r'%(d.indlev,))
   d.indent(-1).write('indlev=%r'%(d.indlev,))
-  d.indent(-1).write('indlev=%r'%(d.indlev,))
+  d.indent(-1).write('indlev=%r'%(d.indlev,)).indstr='  '
+  d.undent()('DISABLING OUTPUT ...').indent()
   prev=d.enable(False)
   d("Disabled debug output (so you shouldn't see this).")
-  print 'Previous DebugChannel enabled state: %r'%(prev,)
+  d.enable(prev)
+  d.undent()('RE-ENABLING OUTPUT ...').indent()
+  d('Previous DebugChannel enabled state: %r'%(prev,))
+  d.undent()('MULTILINE OUTPUT ...\n').indent()
+  d('MULTILINE\nOUTPUT\n...\n')
+  d.undent()('LIST OUTPUT ...').indent()
+  d('LIST OUTPUT ...'.split())
+  d.undent()('TUPLE OUTPUT ...').indent()
+  d(tuple('TUPLE OUTPUT ...'.split()))
+  d.undent()('DICTIONARY OUTPUT ...').indent()
+  d(dict(a='dictionary',c='...',b='output'))
+  d('DONE!')
