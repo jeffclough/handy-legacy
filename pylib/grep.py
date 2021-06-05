3c2b4cdddac051afa1e5a860ab0a196d48e11c4b
Jeff Clough <jeff@cloughcottage.com>
2021-06-05 17:35:35 -0400

    Port grep.py to Python 3

--- pylib/grep.py
+++ pylib/grep.py
@@ -1,10 +1,30 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import re,sys
 
 class Match(object):
   """This is a fairly thin wrapper around the normal _sre.SRE_Match
-  object returned by _sre.SRE_Pattern's match() and search() methods."""
+  object returned by _sre.SRE_Pattern's match() and search() methods.
+  A groupObject() method has been added, which is just like groupDict(),
+  but rather than a dictionary, it retunrs an anonymous object whose
+  attirubtes may be addressed with dot notation.
+  
+  Without the Match class:
+
+      pat=re.compile(r'prefix(?<n>\d+)suffix')
+      m=pat.match(s)
+      if m:
+          print(f"n={m['n']}")
+
+  With the Match class:
+
+      pat=re.compile(r'prefix(?<n>\d+)suffix')
+      m=Match(pat.match(s))
+      if m:
+          print(f"n={m.n}")
+
+  It cleaner to read and much easier to type, especially when dealing
+  with several named groups in the same expression."""
 
   def __init__(self,match):
     self.match=match
@@ -72,7 +92,7 @@ class Match(object):
 
 class Grep(object):
 
-  dummy_pattern=re.compile('dummy')
+  _dummy_pattern=re.compile('dummy')
 
   def __init__(self,arg=None,**kwargs):
     '''Initialize this object. The arg parameter may be string to be
@@ -136,20 +156,20 @@ class Grep(object):
       self.invert=arg.invert
       self.match_all=arg.match_all
       self.value=arg.value
-    elif isinstance(arg,basestring):
+    elif isinstance(arg,str):
       # Assume this string value is an RE to be compiled.
       self.pats=[re.compile(arg,self.flags)]
-    elif isinstance(arg,type(Grep.dummy_pattern)):
+    elif isinstance(arg,type(Grep._dummy_pattern)):
       # Our argument is an already compile RE.
       self.pats=[arg]
-    elif isinstance(arg,list) or isinstance(arg,tuple):
+    elif isinstance(arg,(list,tuple)):
       # Our argument is a sequence of (hopefully) strings and/or
       # compiled RE objects. We'll compile any string values we find as
       # REs.
       for a in arg:
-        if isinstance(a,basestring):
+        if isinstance(a,str):
           self.pats.append(re.compile(a,self.flags))
-        elif isinstance(a,re.RegexObject):
+        elif isinstance(a,type(Grep._dummy_pattern)):
           self.pats.append(a)
         else:
           # As a final effort, try converting this value to a string and then
@@ -172,10 +192,10 @@ class Grep(object):
 
   def __call__(self,input,**kwargs):
     '''This is an iterator that returns all matches from the input
-    argument, which must be an iterable that contains or yields string
-    values. (Non-string values can be managed using the value keyword
-    argument. See __init__()'s description for details.) Tuples returned
-    look like (string,i,[Match,...]) and contain:
+    argument, which must be an iterable of string values. (Non-string
+    values can be managed using the value keyword argument. See
+    __init__()'s description for details.) Tuples returned look like
+    (string,i,[Match,...]) and contain:
 
       1. the whole string that matched
       2. the number (starting with 1) of the matched item in the input
@@ -209,6 +229,8 @@ class Grep(object):
     Filter a list of filenames, leaving only those that match a given
     regular expression:
 
+      import fnmatch
+
       def find_files(root):
         """Return a list of (path,filename) tuples, one for each file
         under the given root path."""
@@ -219,21 +241,21 @@ class Grep(object):
             file_list.append((path,f))
         return file_list
 
-      # Set up a Grep object with the RE that matches the files we want
-      # and which operates on the second element of each tuple in the
-      # list given to it.
-      grep=Grep(args,value=lambda x:x[1])
-
       # Get a list of all (dir,filename) values for each file found
       # under the current directory.
       all_files=find_files('.')
+
+      # Set up a Grep object with the RE that matches the files we want
+      # and which operates on the second element of each tuple in the
+      # list given to it.
+      grep=Grep(fnmatch.translate('foo*.bar'),value=lambda x:x[1])
 
       # Now keep only the files matching "foo*.bar".
       good_files=[item for item,n,matches in grep(all_files)]
 
     [Note that a much better way to accomplish the above assignment
     would be to subclass Grep as DirGrep (for example), whose __call__()
-    operator accepts a root path rather than an input iterable. Ooo.
+    operator accepts a root path rather than an input iterable. Ooooo.
     Maybe I'll build that into the next version of this module. (Yeah.
     Right. Like THAT's gonna happen.)]
     '''
@@ -247,58 +269,58 @@ class Grep(object):
     for kw in kwargs:
       raise TypeError('Grep.grep() got an unexpected keyword argument %r'%(kw,))
   
-    #print 'DEBUG: find_all=%r, match_all=%r, invert=%r'%(find_all,match_all,invert)
-    #print 'DEBUG: value=%r'%value
-    #for p in self.pats: print 'DEBUG: RE: %r'%p.pattern
+    #print('DEBUG: find_all=%r, match_all=%r, invert=%r'%(find_all,match_all,invert))
+    #print('DEBUG: value=%r'%value)
+    #for p in self.pats: print('DEBUG: RE: %r'%p.pattern)
     n=0
     for item in input:
       n+=1
-      #print 'DEBUG:---------------------\nDEBUG: n=%r'%n
+      #print('DEBUG:---------------------\nDEBUG: n=%r'%n)
 
-      #val=value(item) if value else item
-      if value:
-        val=value(item)
-      else:
-        val=item
+      #if value:
+      #  val=value(item)
+      #else:
+      #  val=item
+      val=value(item) if value else item
       results=[Match(p.search(val)) for p in self.pats]
-      #print 'DEBUG: results=%r'%results
+      #print('DEBUG: results=%r'%results)
       lr=len(results)
 
       matches=[m for m in results if m.match]
-      #print 'DEBUG: matches=%r'%matches
+      #print('DEBUG: matches=%r'%matches)
       lm=len(matches)
 
-      #print 'DEBUG: val=%r, lr=%r, lm=%r'%(val,lr,lm)
+      #print('DEBUG: val=%r, lr=%r, lm=%r'%(val,lr,lm))
       if not find_all and (lm==lr if match_all else lm>0)==bool(invert):
         continue
 
+      # We've find one of what we're looking for!
       yield (item,n,matches)
 
 if __name__=='__main__':
-  import optparse,os
-  from OptionParserFormatters import IndentedHelpFormatterWithNL
+  import argparse,os
 
-  op=optparse.OptionParser(
-    formatter=IndentedHelpFormatterWithNL(2,8),
-    usage="%prog [OPTIONS] [FILE] RE ...",
-    description="""This is the test mode of my grep Python module, but you can also use it as a kind of screwey (see the very backward-looking usage above) replacement for your native grep command. Note that this command can be given only one input file, but many regular expressions.""",
+  ap=argparse.ArgumentParser(
+    usage="%(prog)s [OPTIONS] [FILE] RE ...",
+    description="""This is the test mode of my grep Python module, but you can also use it as a kind of screwey (see the very backward-looking usage above) replacement for your native grep command. Note that this command can be given only one input file, but many regular expressions. By default a line of input is regarded as a match if it matches at least one RE.""",
     epilog="""The exit status is 0 if the match was successfull. Otherwise, a value of 1 is returned. If an error occurrs, a value >= 2 is returned. 
 
 If -l is used with --find-files, no filenames are output. The caller must test the exit code to know whether a match was found (or not found, if -v was also given)."""
   )
-  op.add_option('-i',dest='ignore_case',action='store_true',default=False,help="Ingore case.")
-  op.add_option('-l',dest='list_only',action='store_true',default=False,help="Only list the names of files where matches are found. Don't show the actual matches. In this mode, the input file is searched only until the first match.")
-  op.add_option('--find-all',dest='find_all',action='store_true',default=False,help="Output all lines, including those that do not match any pattern.")
-  op.add_option('--match-all',dest='match_all',action='store_true',default=False,help="Matching lines must match all paterns rather than at least one pattern.")
-  op.add_option('--matches-only',dest='matches_only',action='store_true',default=False,help="Show only the matching portion(s) of each line.")
-  op.add_option('-n',dest='line_numbers',action='store_true',default=False,help="Each output line is preceded by its line number, starting at 1.")
-  op.add_option('--find-files',dest='find_files',action='store',default=None,help="Rather than searching standard input, find filenames matching the given regular expressions.")
-  op.add_option('-v',dest='invert',action='store_true',default=False,help="Output non-matching lines rather than matching ones.")
-  opt,args=op.parse_args()
+  ap.add_argument('-i',dest='ignore_case',action='store_true',default=False,help="Ingore case.")
+  ap.add_argument('-l',dest='list_only',action='store_true',default=False,help="Only list the names of files where matches are found. Don't show the actual matches. In this mode, the input file is searched only until the first match.")
+  ap.add_argument('--find-all',dest='find_all',action='store_true',default=False,help="Output all lines, including those that do not match any pattern.")
+  ap.add_argument('--match-all',dest='match_all',action='store_true',default=False,help="Matching lines must match all paterns rather than at least one pattern.")
+  ap.add_argument('--matches-only',dest='matches_only',action='store_true',default=False,help="Show only the matching portion(s) of each line.")
+  ap.add_argument('-n',dest='line_numbers',action='store_true',default=False,help="Each output line is preceded by its line number, starting at 1.")
+  ap.add_argument('--find-files',dest='find_files',action='store',default=None,help="Rather than searching standard input, find filenames matching the given regular expressions.")
+  ap.add_argument('-v',dest='invert',action='store_true',default=False,help="Output non-matching lines rather than matching ones.")
+  ap.add_argument('res',metavar='RE',action='store',nargs='+',help="One or more regular expressions to match against input lines (or filenames if --find-files is given.")
+  opt=ap.parse_args()
 
   def die(msg,rc=1):
     if msg:
-      sys.stderr.write('%s: %s\n'%(op.get_prog_name(),msg))
+      sys.stderr.write('%s: %s\n'%(ap.get_prog_name(),msg))
     sys.exit(rc)
 
   if opt.invert and opt.matches_only:
@@ -327,31 +349,31 @@ If -l is used with --find-files, no filenames are output. The caller must test t
           file_list.append((path,f))
       return file_list
 
-    grep=Grep(args,value=lambda x:x[1],**grep_options)
+    grep=Grep(opt.res,value=lambda x:x[1],**grep_options)
     all_files=find_files(opt.find_files)
     good_files=[dirfile for dirfile,n,matches in grep(all_files)]
-    print '\n'.join([os.path.join(*dirfile) for dirfile in good_files])
+    print('\n'.join([os.path.join(*dirfile) for dirfile in good_files]))
 
   else:
 
     # This mode works kind of like the standard grep command.
 
     # Figure out what the user put on the command line.
-    if len(args)>0 and not sys.stdin.isatty():
+    if len(opt.res)>0 and not sys.stdin.isatty():
       filename='stdin'
       infile=sys.stdin
-    elif len(args)>1:
-      filename=args.pop(0)
+    elif len(opt.res)>1:
+      filename=opt.res.pop(0)
       infile=open(filename)
     else:
-      op.print_usage()
+      ap.print_usage()
       sys.exit(2)
 
     return_code=1
-    for line,n,matches in Grep(args,**grep_options)(infile):
+    for line,n,matches in Grep(opt.res,**grep_options)(infile):
       return_code=0
       if opt.list_only:
-        print filename
+        print(filename)
         break
       prefix=''
       if opt.find_all: prefix+=('> ' if matches else '  ')
