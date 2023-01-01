d147db1773e54f784c9f62c21e5bac4ac38d90be
Jeff Clough <jeff@cloughcottage.com>
2022-12-31 22:16:18 -0500

    More work on table.py
    - Consolidated type-based formatting in Table's formatByType(val, width)
      method. This method used to call various format<Type>(val, width)
      methods, but there was so little work done in those specialized
      functions, it's simpler to just let formatByType do the work.
    - Added logic to provide base-26 column names if none are given when a
      new Table is instantiated.
    - Added some decent test data for use in testing mode.

--- pylib/table.py
+++ pylib/table.py
@@ -5,8 +5,8 @@ another list. It is suitable for tabular data and can be manipulated
 as such. A Table can optionally have column names, but they are not
 included in the Table's data. Column names are case-sensitive.
 
-One major difference between a Table and a list[][] is that a the
-rows of a Table MUST all be the same length.
+One major difference between a Table and a list[][] is the rows of a
+Table MUST all be the same length.
 
 t=Table(columns=5)
 
@@ -14,7 +14,7 @@ t=Table(columns='Name Street City State Zip'.split())
 
 t.append(row)
 
-t.extend(iterable_producing_row_values)
+t.extend(iterable_producing_row_values(...))
 
 t.count() returns number of rows.
 
@@ -24,22 +24,33 @@ t.pop(index=-1)
 
 t.reverse()
 
-t.sort(cmp=None,key-=None,reverse=False)
+t.sort(cmp=None,key=None,reverse=False)
 
 t.ouput(format='table',stream=sys.stdout,dialect='box') <-- the defalt
 t.ouput(format='table',stream=sys.stdout,dialect='markdown')
-t.ouput(format='csv',stream=sys.stdout,dialect=',r"mt\\f\n')
+t.ouput(format='csv',stream=sys.stdout,dialect=',r'mt\\f\\n')
 t.ouput(format='json',stream=sys.stdout)
 
 t[row_num] refers directly to that row (not a copy).
 t[row_num][col_num] refers directly to that cell of the table (not a copy).
 t[row_num][col_name] refers directly to that cell of the table (not a copy).
-t[col_name] returns a copy of that column's values.
+t[col_name] returns a COPY of that column's values.
 
 
 """
 
 import sys
+
+def base26(n):
+  "Return 'A' for n=0, 'B' for n=1, and so forth."
+
+  s=''
+  while True:
+    n,r=divmod(n,26)
+    s=chr(65+r)+s
+    if not n:
+      break
+  return s
 
 class Table(list):
 
@@ -52,11 +63,12 @@ class Table(list):
 
   class Row(list):
     """A Table.Row object is just like a Python list, but it knows what
-    Table instance it belongs to, which means that it knows what length
-    it should be.
+    Table instance it belongs to, which means it knows what length it
+    should be, and there's a name for each of its elements, so they can
+    be addressed by zero-based index or my name.
 
-    Instantiating Table.Row does NOT add the new instance to its table.
-    Use Table's insert() or append() methods for that."""
+    Instantiating t.Row using Table t does NOT add the new instance to
+    t. Use Table's t.insert() or t.append() methods for that."""
 
     def __init__(self,table,data=None):
       if not isinstance(table,Table):
@@ -77,41 +89,49 @@ class Table(list):
         list.__init__(self,data)
 
     def append(self,value):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT change length.')
+      super().append(value)
 
     def extend(self,iterable):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT change length.')
+      super().extend(iterable)
 
     def insert(self,index,value):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT change length.')
+      super().insert(index,value)
 
     def pop(self,index=-1):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT change length.')
+      super().pop(index)
 
     def remove(self,value):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT change length.')
+      super().remove(value)
 
     def reverse(self):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT be reversed.')
+      super().reverse()
 
     def sort(self,cmp=None,key=None,reverse=False):
-      if not self.owner.internal_change:
+      if not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT be sorted.')
+      super().sort(cmp,key,reverse)
 
     def __setslice__(self,i,j,seq):
-      "Replace self[i:j] with y IFF len(y)==j-i."
+      "Replace self[i:j] with seq IFF len(seq)==j-i."
 
-      if len(y)==j-y:
-        for c in range(i,j):
-          self[c]=seq[c-i]
-      else:
+      if len(seq)!=j-i and not self.owner._internal_change:
         raise Table.Error('Table rows MUST NOT change length.')
+#     if len(seq)==j-i:
+#       for c in range(i,j):
+#         self[c]=seq[c-i]
+      super().__setslice__(i,j,seq)
 
   #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
@@ -121,6 +141,10 @@ class Table(list):
     self.colcount=colcount
 
     self._validate_columns()
+
+    # This _internal_change value will be True only to allow the lengths of our
+    # rows to change, e.g. while adding or removing a column. Keep this in mind
+    # if using a Table in a multi-threaded environment.
     self._internal_change=False
 
   def _validate_columns(self,set_colcount=None):
@@ -128,91 +152,76 @@ class Table(list):
     if self.colcount is None and set_colcount is not None:
       self.colcount=set_colcount
 
-    # Ensure colnames makes sense. Initialize colcount from it if needed.
-    if self.colnames is not None:
+    if self.colnames:
+      # Ensure colnames makes sense. Initialize colcount from it if needed.
       if not isinstance(self.colnames,(tuple,list)):
         raise Table.Error('If given, colnames argument MUST be a tuple or a list!')
       self.colnames=[str(x) for x in self.colnames]
-      if self.colcount is None:
+      if not self.colcount:
         self.colcount=len(self.colnames)
-      elif selfcolcounrt!=len(self.colnames):
-        raise Table.Error(f"{len(self.colnames)} column names not compatible with column count of {self.colcount}!")
-
+      elif self.colcount!=len(self.colnames):
+        raise Table.Error(
+          f"{len(self.colnames)} column names not compatible "
+          "with column count of {self.colcount}!"
+        )
+    elif self.colcount:
     # Ensure colcount makes sense. Initialize colnames from it if needed.
-    if self.colcount is not None:
       if not isinstance(self.colcount,int):
         raise Table.Error('colcount argument must be an integer!')
       if not self.colnames:
-        # TODO: Provide logic for more than 26 columns.
-        self.colnames=[f"{ord(65+c)}" for c in range(self.colcount)]
+        self.colnames=[base26(c) for c in range(self.colcount)]
 
-    # Ensure we can identify our columns by name or by number.
-    #self.colid=dict([(self.colnames[c],c) for c in range(self.colcount)])
+    # Ensure we can identify our columns both by name and by number.
     self.colid={self.colnames[c]:c for c in range(self.colcount)}
-    #self.colid.update(dict([(c,c) for c in range(self.colcount)]))
     self.colid.update({c:c for c in range(self.colcount)})
+
+   # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # Row operations specialized to distinguish Table from list.
 
   def append(self,row):
     row=Table.Row(self,row)
-    if self.colcount==None:
+    if not self.colcount:
       self._validate_columns(len(row))
     list.append(self,row)
 
   def insert(self,index,row):
     row=Table.Row(self,row)
-    if self.colcount==None:
+    if not self.colcount:
       self._validate_columns(len(row))
     list.insert(self,index,row)
 
   def extend(self,rows):
     rows=[Table.Row(self,r) for r in rows]
-    if self.colcount==None:
+    if not self.colcount:
       self._validate_columns(max([len(row) for row in rows]))
     list.extend(self,rows)
 
   def pop(self,index=-1):
-    if len(self)==0:
+    if not len(self):
       raise Table.Error('Cannot pop from empty table')
     return list.pop(self,index)
 
-  #//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
-  # Override one or more of these methods to change how different types are
-  # formatted in columns.
-
-  def formatInt(self,val,width=None):
-    """Return this integer as a string. If width is not None, it must
-    be an integer giving the number of chacters the string must contain."""
-
-    if width==None: return str(val)
-    return '%*d'%(width,val)
-
-  def formatFloat(self,val,width=None):
-    """Return this float as a string. If width is not None, it must be
-    an integer giving the number of chacters the string must contain."""
-
-    if width==None: return str(val)
-    return '%*g'%(width,val)
-
-  def formatString(self,val,width=None):
-    """Return this string unmodified if width is None. Otherwise,
-    return it formatted in the given width, which must be an integer."""
-
-    return val if width is None else '%-*s'%(width,val)
-
-  def formatOther(self,val,width=None):
-    """Do our best to convert this value to a string and then
-    return it formatted as such. If you'd like to handle certain
-    types differently, override this method in your own subclass."""
-
-    return self.formatString(str(val),wdith)
-
-  #//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+   # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
   def formatByType(self,val,width=None):
-    if isinstance(val,int): return self.formatInt(val,width)
-    if isinstance(val,float): return self.formatFloat(val,width)
-    if isinstance(val,str): return self.formatString(val,width)
-    return self.formatOther(val,width)
+    """Format the given value according to its type and the given width.
+    If width is not given, it is formatted without padding. Numeric
+    values are right-justified. All others are left-justified. Override
+    this method if you prefer more customized formatting."""
+
+    # Right-justify numbers and left-justify everything else.
+    if isinstance(val,int):
+      s=f"{val:>{width}d}" if width else str(val)
+    elif isinstance(val,float):
+      s=f"{val:>{width}g}" if width else str(val)
+    elif isinstance(val,str):
+      s=f"{val:<{width}s}" if width else str(val)
+    else:
+      # We don't recognize this type, but maybe it knows how to stringify itself.
+      s=f"{str(val):<{width}s}" if width else str(val)
+    return s
 
   def output(self,dialect='box',stream=sys.stdout):
     if dialect in ('ascii','box'):
@@ -229,13 +238,10 @@ class Table(list):
       for c in range(self.colcount):
         widths[c]=max(widths[c],max([len(self.formatByType(row[c])) for row in self]))
       # Write the header lines.
-      #stream.write((' | '.join([self.colnames[c].center(widths[c]) for c in range(self.colcount)]))+'\n')
-      #stream.write(('-+-'.join(['-'*widths[c] for c in range(self.colcount)]))+'\n')
       print(div.join([self.colnames[c].center(widths[c]) for c in range(self.colcount)]),file=stream)
       print(hdiv.join([hline*widths[c] for c in range(self.colcount)]),file=stream)
       # Write the body of this table.
       for row in self:
-        #stream.write((' | '.join([self.formatByType(row[c],widths[c]) for c in range(self.colcount)]))+'\n')
         print(div.join([self.formatByType(row[c],widths[c]) for c in range(self.colcount)]),file=stream)
     elif dialect=='markdown':
       pass
@@ -243,4 +249,51 @@ class Table(list):
       raise Table.Error(f"Unrecognized Table output dialect: {dialect!r}")
 
 if __name__=='__main__':
-  t=Table()
+  import CSV as csv
+  from pprint import pprint
+
+  # Annual gas use for my 2007 Yaris.
+  test_data_lines="""\
+Year,Tanks,Miles,Galons,$/Gal,Total Cost
+2007,78,26760,729.23,2.65,1931.64
+2008,85,26522,705.6,3.17,2235.73
+2009,79,26333,725.05,2.11,1527.62
+2010,71,23997,675.41,2.58,1745.51
+2011,83,27435,774.08,3.41,2636.03
+2012,89,27509,800.23,3.5,2803.84
+2013,86,26969,795.1,3.41,2711.69
+2014,79,24139,733.85,3.23,2371.21
+2015,77,23537,737.36,2.3,1699.14
+2016,85,25435,815.09,2.05,1671.76
+2017,81,23219,744.2,2.26,1681.05
+2018,67,19648,632.2,2.6,1646.36
+2019,68,20246,645.24,2.46,1588.2
+2020,33,9780,304.18,1.98,602.66
+2021,31,8800,273.02,2.8,765.27
+2022,32,9573,306.37,3.6,1103.53
+this,is,a,test,of,strings""".split('\n')
+
+  def numeric(val):
+    "Return the numeric version of this value if possible."
+
+    try:
+      return int(val)
+    except ValueError:
+      try:
+        return float(val)
+      except:
+        pass
+    if type(val)!=str:
+      val=str(val)
+    return val
+
+  reader=csv.reader(test_data_lines)
+  test_data=[row for row in reader]
+  columns=test_data[0]
+  data=[[numeric(c) for c in row] for row in test_data[1:]]
+
+  t=Table(columns)
+  t.extend(data)
+  t.output()
+  print()
+  t.output(dialect='ascii')
